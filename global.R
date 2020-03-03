981b65fa691ee694cf3d978f1b8971eb064934d3
Jorge Cornejo <cornejotux@gmail.com>
2020-03-03 13:19:24 -0300

    Data added and added the transformation from wide to long data format.

--- global.R
+++ global.R
@@ -6,26 +6,36 @@
 ##### This section download the data from KNB and prepare it for Shiny app
 rm(list=ls())
 
-library(data.table)
+#library(data.table)
 library(dplyr)
-require(lubridate)
-require(ggplot2)
-require(ggjoy)
-require(scales)
-library(ggthemes)
-library(shinythemes)
-library(shinyjs)
-library(gridExtra)
+#library(tidyr)
+#require(lubridate)
+#require(ggplot2)
+#require(ggjoy)
+#require(scales)
+#library(ggthemes)
+#library(shinythemes)
+#library(shinyjs)
+#library(gridExtra)
 
 # Use the file that produce the data for this app.
 
-escape <- readRDS(file="meanEscapement.RDS")
+#escape <- readRDS(file="meanEscapement.RDS")
 
+# chl <- read.csv(file="data/CHLA.csv", sep=";", dec=",", na.strings = "NaN", stringsAsFactors = F)
+#   
+# namesSt <-  names(chl) 
+# data_long <- pivot_longer(chl, namesSt[4:length(namesSt)])
+# 
+# rm(chl, namesSt)
 
-selectYears <- function(data=escape)
+load("~/Personales/patriciafaundez/ShinyChla/data/dataChl.RData")
+data_long <- rename(data_long, estacion = name, chl = value)
+
+selectYears <- function(data=data_long)
 {
-    minY <- min(data$sampleYear, na.rm = T)
-    maxY <- max(data$sampleYear, na.rm = T)
+    minY <- min(data$Year, na.rm = T)
+    maxY <- max(data$Year, na.rm = T)
     output <- c(minY, maxY)
     return(output)
 }
@@ -33,3 +43,5 @@ selectYears <- function(data=escape)
 R <<- NULL
 S <<- NULL
 tab <<- 0
+
+estaciones <- unique(data_long$estacion)
